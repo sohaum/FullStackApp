7beb88607d66dbd21ce1067b9f4ce8ea5d8c13dc
sohaum <127203784+sohaum@users.noreply.github.com>
2025-06-02 14:06:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,3 +11,14 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// MONGO=mongodb+srv://sohaumghosh:sohaumghosh@cluster0.gcduaks.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0
+
+// GOOGLE_CLIENT_ID=499113140558-bb0u37qs0ksm7asb2cmkogvs95pd896o.apps.googleusercontent.com
+// GOOGLE_CLIENT_SECRET=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
+
+// NEXTAUTH_SECRET="sohaumghosh"
+// NEXTAUTH_URL=http://localhost:3000
+
+// GITHUB_ID=Ov23liTiIrL1wUPKDJtu
+// GITHUB_SECRET=45a58b4867d646a2d6796492ff631ba6e19f728a
